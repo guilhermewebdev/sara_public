d8c79a39d88e73a32310d12fc89505858f88cf2f
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-05-06 16:17:00 -0300

    Criação do entrypoint do coletor

--- controllers/sara_coletor.py
+++ controllers/sara_coletor.py
@@ -21,27 +21,15 @@ from ..core.logger import log
 # nome_banco="eleicao"
 
 # termo colecao numero_tweets
-try:
-    name_file = sys.argv[0]
-    termo = sys.argv[1]
-    n_tweets = sys.argv[2]
-    colecao = sys.argv[3]
-    nome_banco = sys.argv[4]
-except IndexError as exc:
-    print(f"error {exc}")
-    print(f"ERRO!Digite {name_file} <termo>"
-          "<numero_tweets> <colecao> <banco de dados>")
-    print("\nTermo: O termo a ser coletado" +
-          "\nNúmero de Tweets: número de tweets a ser coletado." +
-          "0 para definir sem limite" +
-          "\nColecao: A coleção onde os tweets serão salvos" +
-          "\nBanco de Dados: O  banco onde os dados serão armazenados")
-
-    sys.exit()
-
-
-log(termo)
-coletor = Sauron()
-coletor.pesquisa(termo, n_tweets, colecao, nome_banco)
+def coletar(
+    name_file,
+    termo,
+    n_tweets,
+    colecao,
+    nome_banco
+):
+    log(termo)
+    coletor = Sauron()
+    coletor.pesquisa(termo, n_tweets, colecao, nome_banco)
 
 # odetalhista.detector_bots(nome_banco,colecao)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask, request
 
 from controllers.sara_centralidade import Importancia
+from controllers.sara_coletor import coletar
 
 app = Flask(__name__)
 
@@ -20,6 +21,27 @@ def centralidade():
         response['sucess'] = True
     except KeyError as err:
         response['error'] = err
+        response['sucess'] = False
+    else:
+        response['sucess'] = False
+    finally:
+        return response
+
+@app.route('/coletor', methods=['POST'])
+def coletor():
+    response = {}
+    try:
+        coletar(
+            name_file=request.form['arquivo'],
+            termo=request.form['termo'],
+            n_tweets=request.form['n_tweets'],
+            colecao=request.form['colecao'],
+            nome_banco=request.form['banco']
+        )
+    except KeyError as err:
+        response['error'] = err
+        response['sucesss'] = False
+    else:
         response['sucess'] = False
     finally:
         return response
